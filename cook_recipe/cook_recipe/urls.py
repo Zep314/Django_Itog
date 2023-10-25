512f578a46da86cc468f5f6daffae5af6df96879
Dmitry Mordanov <mordanov314@gmail.com>
2023-10-25 17:44:59 +0300

    work 25.10.2023_2

--- cook_recipe/cook_recipe/urls.py
+++ cook_recipe/cook_recipe/urls.py
@@ -18,6 +18,7 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.contrib.auth import views as auth_views
 import sys
 sys.path.append("..")
 from users import views
@@ -26,6 +27,8 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('register/', views.register, name='register'),
     path('', include('website.urls')),
-
+    path('profile/', views.profile, name='profile'),
+    path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
 ]  + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
