e341a6639efa63880ae07beb2305c68234baee8b
Dmitry Mordanov <mordanov314@gmail.com>
2023-11-15 22:19:22 +0300

    deploy2

--- cook_recipe/cook_recipe/settings.py
+++ cook_recipe/cook_recipe/settings.py
@@ -36,7 +36,7 @@ CSRF_COOKIE_SECURE = True
 
 ALLOWED_HOSTS = [
     '127.0.0.1',
-    'zep314.pythonanywhere.com',
+    'zep315.pythonanywhere.com',
 ]
 
 
@@ -106,10 +106,10 @@ WSGI_APPLICATION = 'cook_recipe.wsgi.application'
 DATABASES = {
          'default': {
              'ENGINE': 'django.db.backends.mysql',
-             'NAME': 'Zep314$default',
-             'USER': 'Zep314',
+             'NAME': 'Zep315$default',
+             'USER': 'Zep315',
              'PASSWORD': os.getenv('MYSQL_PASSWORD'),
-             'HOST': 'Zep314.mysql.pythonanywhere-services.com',
+             'HOST': 'Zep315.mysql.pythonanywhere-services.com',
              'OPTIONS': {
                  'init_command': "SET NAMES 'utf8mb4';SET sql_mode='STRICT_TRANS_TABLES'",
                  'charset': 'utf8mb4',
