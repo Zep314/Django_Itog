2237e6e4a58c333ada4acaedc5a5c7777510487e
Dmitry Mordanov <mordanov314@gmail.com>
2023-11-15 16:28:39 +0300

    ready to deploy 1

--- cook_recipe/cook_recipe/settings.py
+++ cook_recipe/cook_recipe/settings.py
@@ -22,12 +22,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-#5m5w8tkzy(cv=emy%t8e42pjpz_*xdu_&5l03xym9ciau9p=&'
-#SECRET_KEY = os.getenv('SECRET_KEY')
+#SECRET_KEY = 'django-insecure-#5m5w8tkzy(cv=emy%t8e42pjpz_*xdu_&5l03xym9ciau9p=&'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 # Развертывание проекта.
 SESSION_COOKIE_SECURE = True
@@ -96,26 +96,26 @@ WSGI_APPLICATION = 'cook_recipe.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-
 # DATABASES = {
-#          'default': {
-#              'ENGINE': 'django.db.backends.mysql',
-#              'NAME': 'Zep314$default',
-#              'USER': 'Zep314',
-#              'PASSWORD': os.getenv('MYSQL_PASSWORD'),
-#              'HOST': 'Zep314.mysql.pythonanywhere-services.com',
-#              'OPTIONS': {
-#                  'init_command': "SET NAMES 'utf8mb4';SET sql_mode='STRICT_TRANS_TABLES'",
-#                  'charset': 'utf8mb4',
-#              },
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
 #     }
 # }
+
+DATABASES = {
+         'default': {
+             'ENGINE': 'django.db.backends.mysql',
+             'NAME': 'Zep314$default',
+             'USER': 'Zep314',
+             'PASSWORD': os.getenv('MYSQL_PASSWORD'),
+             'HOST': 'Zep314.mysql.pythonanywhere-services.com',
+             'OPTIONS': {
+                 'init_command': "SET NAMES 'utf8mb4';SET sql_mode='STRICT_TRANS_TABLES'",
+                 'charset': 'utf8mb4',
+             },
+    }
+}
 
 
 
